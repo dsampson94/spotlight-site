7d839aff0a63491ee6a0744d701f5c0347e67bf8
dsampson94 <davesampson15@gmail.com>
2024-04-08 18:01:43 +0200

    adds updated cv

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,32 +1,38 @@
+import Head from 'next/head';
+
 import { Providers } from '@/app/providers';
 import { MainLayout } from '@/components/MainLayout';
 
-import '@/styles/tailwind.css'
+import '@/styles/tailwind.css';
 
 export const metadata = {
     title: {
         template: '%s',
         default:
-            'Tech Notes - Home',
+            'Tech Notes - Home'
     },
     description: '%s',
     alternates: {
         types: {
-            'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-        },
-    },
-}
+            'application/rss+xml': `${ process.env.NEXT_PUBLIC_SITE_URL }/feed.xml`
+        }
+    }
+};
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
     return (
         <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+        <Head>
+            <meta property="og:image" content={ 'https://technotes.site/public/headshot.jpg' } />
+            <meta property="og:type" content="website" />
+        </Head>
         <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
             <div className="flex w-full">
-                <MainLayout>{children}</MainLayout>
+                <MainLayout>{ children }</MainLayout>
             </div>
         </Providers>
         </body>
         </html>
-    )
+    );
 }
