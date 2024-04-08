607c55f492e16bc82c079da97baebf87c5d2bc15
dsampson94 <davesampson15@gmail.com>
2024-04-08 14:14:48 +0200

    adds updated cv

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -164,7 +164,7 @@ function Resume() {
                 <ArrowDownIcon
                     className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
             </Button>
-            <Button href={ '/cv' } target="_blank" variant="secondary" className="group mt-6 w-full">
+            <Button href={ '/legacy-cv' } target="_blank" variant="secondary" className="group mt-6 w-full">
                 View Legacy Web CV
                 <ArrowDownIcon
                     className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
