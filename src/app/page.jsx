481742ebbcb2fb48999426700617745b3aa99245
dsampson94 <67152635+dsampson94@users.noreply.github.com>
2024-06-11 10:28:53 +0200

    typography

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -159,7 +159,7 @@ export default async function Home() {
             <Container className="mt-9">
                 <div className="max-w-1xl">
                     <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                    Welcome to the digital home of my tech related creations.
+                    Software Engineer & JavaScript Enthusiast
                     </p>
                     <div className="mt-6 flex gap-6">
                         <SocialLink
