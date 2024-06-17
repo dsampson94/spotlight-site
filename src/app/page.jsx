3dd0269a667774c8285e18f0599b141677220e0b
dsampson94 <67152635+dsampson94@users.noreply.github.com>
2024-06-17 09:55:32 +0200

    cv update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default async function Home() {
             <Container className="mt-9">
                 <div className="max-w-1xl mx-auto">
                     <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-                        Software lover, aspiring founder, and full time javascript tinkerer
+                        Software lover, aspiring founder, and full time javascript builder.
                     </h1>
                     <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
                         I’m David, a software developer living between Cape Town and Johannesburg, I&apos;ve been working professionally
