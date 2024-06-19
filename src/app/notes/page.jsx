54bb6b6430d4f742269d146aea1f507f4b0b6419
dsampson94 <67152635+dsampson94@users.noreply.github.com>
2024-06-19 16:20:26 +0200

    notes change

--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -43,7 +43,7 @@ export default async function ArticlesIndex() {
 
   return (
       <SimpleLayout
-          // title="Writing on software development, agile, web design, latest tech and how best practices can be used for good."
+          title="All my public notes, Idea phrases and insights - further developed with GPT4."
           // intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
       >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
