8bec52fab06de299fabb5167143a3bbaca87b3de
dsampson94 <67152635+dsampson94@users.noreply.github.com>
2024-06-19 14:23:22 +0200

    notes change

--- src/app/feed.xml/route.js
+++ src/app/feed.xml/route.js
@@ -29,18 +29,18 @@ export async function GET(req) {
   })
 
   let articleIds = require
-    .context('../articles', true, /\/page\.mdx$/)
+    .context('../notes', true, /\/page\.mdx$/)
     .keys()
     .filter((key) => key.startsWith('./'))
     .map((key) => key.slice(2).replace(/\/page\.mdx$/, ''))
 
   for (let id of articleIds) {
-    let url = String(new URL(`/articles/${id}`, req.url))
+    let url = String(new URL(`/notes/${id}`, req.url))
     let html = await (await fetch(url)).text()
     let $ = cheerio.load(html)
 
-    let publicUrl = `${siteUrl}/articles/${id}`
-    let article = $('article').first()
+    let publicUrl = `${siteUrl}/notes/${id}`
+    let article = $('note').first()
     let title = article.find('h1').first().text()
     let date = article.find('time').first().attr('datetime')
     let content = article.find('[data-mdx-content]').first().html()
