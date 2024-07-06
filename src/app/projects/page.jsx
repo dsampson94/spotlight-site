2a4a7c46ce7c4c8b45fe9477c359e10c105a7439
dsampson94 <davesampson15@gmail.com>
2024-07-06 14:14:01 +0200

    adds optimized videos

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -80,7 +80,7 @@ const projects = [
 
 function MediaGallery({ media }) {
   return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
         {media.map((item, index) => (
             item.type === 'image' ? (
                 <img key={index} src={item.url} alt={`media-${index}`} className="w-full h-48 object-cover rounded-lg" />
@@ -95,31 +95,18 @@ function MediaGallery({ media }) {
   );
 }
 
-function LinkIcon(props) {
-  return (
-      <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-        <path
-            d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
-            fill="currentColor"
-        />
-      </svg>
-  );
-}
-
 export const metadata = {
   title: 'David Sampson - Projects',
-  description: 'Things I’ve made trying to put my dent in the universe.',
+  description: 'Applications I’ve worked on trying to put my dent in the universe.',
 };
 
 export default function Projects() {
   return (
       <SimpleLayout
-          title="Professional Projects I have had the fortune of working on."
+          title="Professional Projects I have had the fortune of working on, in chronological order."
+          // intro="Trying to put my dent in the universe."
       >
-        <ul
-            role="list"
-            className="space-y-16"
-        >
+        <ul role="list" className="space-y-16">
           {projects.map((project) => (
               <Card as="li" key={project.name} className="space-y-4">
                 <h2 className="text-lg font-semibold text-zinc-800 dark:text-zinc-100">
